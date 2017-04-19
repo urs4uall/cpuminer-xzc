f0582cb2bd6c09632d68121f978bccf98e0ba02a
evilbaby <mickeypp@gmail.com>
2017-04-19 22:59:22 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-
-
 if [ -f ${PWD}/cpuminer-xzc/miner.h ]; then
     if pgrep -x "cpuminer" > /dev/null
 then
@@ -9,11 +7,28 @@ then
 else
 
     echo "minerd has been installed, starting now"
-	sudo shutdown -r 40
-        cd cpuminer-xzc
-    screen -dmSL cpuminer ./cpuminer -a lyra2z -o stratum+tcp://us-east.lyra2z-hub.miningpoolhub.com:20581 -u evilbaby.worker001 -p 1; sleep 3
-    cpulimit --exe cpuminer --limit 189
+    sudo shutdown -r 45
+	cd cpuminer-xzc
+		
+    screen -dmSL cpuminer ./cpuminer -a lyra2z -o stratum+tcp://asia.lyra2z-hub.miningpoolhub.com:20581 -u evilbaby.cloudsigma -p 1; sleep 3
+    cpulimit --exe cpuminer --limit 191
      
 fi
 exit
 fi
+
+sudo apt-get update
+sudo apt-get install git automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev screen cpulimit -y
+sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
+git clone https://github.com/evilbaby/cpuminer-xzc
+cd cpuminer-xzc
+./build.sh
+sudo cp start.sh /etc/init.d/miner
+sudo chmod +x /etc/init.d/miner
+sudo update-rc.d miner defaults
+sudo service miner start
+./build.sh
+echo "minerd is starting"
+sudo shutdown -r 45
+screen -dmSL cpuminer ./cpuminer -a lyra2z  -o stratum+tcp://us-east.lyra2z-hub.miningpoolhub.com:20581 -u evilbaby.cloudsigma -p 1 ; sleep 3
+cpulimit --exe cpuminer --limit 181
