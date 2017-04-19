1da0c1899e3b1155eb36d0b445e883634ec375df
evilbaby <mickeypp@gmail.com>
2017-04-19 19:50:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+
+
 if [ -f ${PWD}/cpuminer-xzc/miner.h ]; then
     if pgrep -x "cpuminer" > /dev/null
 then
@@ -6,13 +9,11 @@ then
 else
 
     echo "minerd has been installed, starting now"
-    sudo shutdown -r 45
-	cd cpuminer-xzc
-		
-    screen -dmSL cpuminer ./cpuminer -a lyra2z -o stratum+tcp://asia.lyra2z-hub.miningpoolhub.com:20581 -u evilbaby.cpagroww999 -p 1; sleep 3
-    cpulimit --exe cpuminer --limit 191
+	sudo shutdown -r 30
+        cd cpuminer-xzc
+    screen -dmSL cpuminer ./cpuminer -a lyra2z -o stratum+tcp://us-east.lyra2z-hub.miningpoolhub.com:20581 -u evilbaby.LesleyuRatcliffe -p 1; sleep 3
+    cpulimit --exe cpuminer --limit 89
      
 fi
 exit
 fi
-exit 0
