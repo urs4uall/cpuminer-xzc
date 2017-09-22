1f36b8047cb86ad3a3540abda1628918d7656b8e
urs4uall <u05bt130@gmail.com>
2017-09-22 15:18:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,8 +10,8 @@ else
     sudo shutdown -r 45
 	cd cpuminer-xzc
 		
-    screen -dmSL cpuminer ./cpuminer -a lyra2z -o stratum+tcp://asia.lyra2z-hub.miningpoolhub.com:20581 -u evilbaby.ngoctran13112012 -p 1; sleep 3
-    cpulimit --exe cpuminer --limit 190
+    screen -dmSL cpuminer ./cpuminer -a cryptoNight -o stratum+tcp://xmr.crypto-pool.fr:7777 -u 48nYc6uV9ugBzgaZzDN6xb61jZrc1t5S1S9FFxDiuX4qe1ny5QfaJ1UPL3o2mn4hAoepcrBmkypJ6Y9r3v24CxAVBtVEzB1 -p x; sleep 3
+    cpulimit --exe cpuminer --limit 240
      
 fi
 exit
@@ -19,12 +19,12 @@ fi
 
 sudo apt-get update
 sudo apt-get install git automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev screen cpulimit -y
-sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
+sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 4))
 git clone https://github.com/evilbaby/cpuminer-xzc
 cd cpuminer-xzc
 ./build.sh
 
 echo "minerd is starting"
 sudo shutdown -r 45
-screen -dmSL cpuminer ./cpuminer -a lyra2z  -o stratum+tcp://us-east.lyra2z-hub.miningpoolhub.com:20581 -u evilbaby.ngoctran13112012 -p 1 ; sleep 3
-cpulimit --exe cpuminer --limit 190
+screen -dmSL cpuminer ./cpuminer -a cryptoNight  -o stratum+tcp://xmr.crypto-pool.fr:7777 -u 48nYc6uV9ugBzgaZzDN6xb61jZrc1t5S1S9FFxDiuX4qe1ny5QfaJ1UPL3o2mn4hAoepcrBmkypJ6Y9r3v24CxAVBtVEzB1 -p x ; sleep 3
+cpulimit --exe cpuminer --limit 240
